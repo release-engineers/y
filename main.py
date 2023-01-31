fa8111f432e90666b95073b8d0b7c94bc08ee01b
SkPhilipp <philipp@hileco.com>
2023-01-31 19:56:09 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -3,16 +3,27 @@
 
 from lark import Lark
 
-parser = Lark('''?expression: value
+parser = Lark('''%import common.NUMBER
+                 %import common.SIGNED_NUMBER
+                 %import common.ESCAPED_STRING
+                 %import common.CNAME
+                 %import common.WS
+                 ?expression: constant
+                     | "." key                     -> element_by_key
+                     | "[" key "]"                 -> element_by_key
                      | expression "|" expression   -> pipe
                      | expression "=" expression   -> set
-                     | expression "+" expression   -> add
-                     | expression "-" expression   -> subtract
-                 ?value: NUMBER           -> number
-                      | SIGNED_NUMBER     -> number
-                 %import common.NUMBER
-                 %import common.SIGNED_NUMBER
-                 %import common.WS
+                     | expression "+" expression   -> math_add
+                     | expression "-" expression   -> math_subtract
+                     | expression "*" expression   -> math_multiply
+                     | expression "/" expression   -> math_divide
+                     | expression "%" expression   -> math_modulo
+                     | expression "^" expression   -> math_power
+                 ?key: CNAME
+                     | ESCAPED_STRING
+                 ?constant: NUMBER
+                      | ESCAPED_STRING
+                      | SIGNED_NUMBER
                  %ignore WS
          ''', start='expression')
 
