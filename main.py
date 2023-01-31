a93a9b387f8ed2d417898b98cd1332be3225bce4
SkPhilipp <philipp@hileco.com>
2023-01-31 23:13:23 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ parser = Lark('''%import common.NUMBER
 
                  ?math_prio3: math_prio3 /\\+|-/ math_prio2          -> math_prio3
                       | math_prio2
-                 ?math_prio2: math_prio2 /\\/|\\*|%/ math_prio1    -> math_prio2
+                 ?math_prio2: math_prio2 /\\/|\\*|%/ math_prio1      -> math_prio2
                       | math_prio1
-                 ?math_prio1: constant /\\^/ math_prio1                -> math_prio1
+                 ?math_prio1: constant /\\^/ math_prio1              -> math_prio1
                      | constant
                      | reference
 
@@ -34,17 +34,13 @@ parser = Lark('''%import common.NUMBER
                       | "false"                                      -> boolean_false
                       | "null"                                       -> null
 
-                 ?reference: "$"                                     -> reference_root
-                     | "$" subreference                              -> reference_root_with_subreference
-                     | "."                                           -> reference_contextual
-                     | subreference                                  -> reference_contextual_with_subreference
+                 ?reference: "$" subreference*                       -> reference_root
+                     | "."                                           -> reference_context
+                     | subreference+                                 -> reference_context_subreference
 
                  ?subreference: "." key                              -> field
                     | "[" index "]"                                  -> array_element
                     | "["  "]"                                       -> array
-                    | "." key subreference                           -> field_with_subreference
-                    | "[" index "]" subreference                     -> array_element_with_subreference
-                    | "["  "]" subreference                          -> array_with_subreference
 
                  ?index: NUMBER                                      -> index
 
@@ -52,16 +48,17 @@ parser = Lark('''%import common.NUMBER
                      | ESCAPED_STRING
 
                  %ignore WS
-         ''', start='pipe')
+         ''', start='pipe', propagate_positions=True)
 
 
 def demo(expression):
     print(expression)
-    print(parser.parse(expression).pretty())
+    parsed = parser.parse(expression)
+    print(parsed.pretty())
 
 
 demo('.a.b.c[0] = 123')
-demo('.a.b | .c[0]')
+demo('.a | .b | .c | [0]')
 demo('"abcdefg"')
 demo('true')
 demo('false')
