6c9a2e1515c01e007a545f582285e5dc97d0cc2d
SkPhilipp <philipp@hileco.com>
2023-01-31 19:45:28 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from lark import Lark
+
+parser = Lark('''?expression: value
+                     | expression "|" expression   -> pipe
+                     | expression "=" expression   -> set
+                     | expression "+" expression   -> add
+                     | expression "-" expression   -> subtract
+                 ?value: NUMBER           -> number
+                      | SIGNED_NUMBER     -> number
+                 %import common.NUMBER
+                 %import common.SIGNED_NUMBER
+                 %import common.WS
+                 %ignore WS
+         ''', start='expression')
+
+res = parser.parse('2 + 3 - 4').pretty()
+print(res)
